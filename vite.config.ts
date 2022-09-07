180aa906fbb901a6d43cf63a7187f07f35d48a0d
SantiagoGBe <santiago.guzman@digitalcomtech.com>
2022-09-07 10:05:04 -0500

    first commit vite vue electron and eslint configurated

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: path.resolve(__dirname, './dist'),
+  plugins: [vue()]
+})
+
+
